193c76fb53e2b4837af2020b110053af7d764eeb
Yutaka Masuda <masuday@blup.sakura.ne.jp>
2018-04-09 19:01:50 -0400

    initial commit

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,16 @@
 using AnimalBreedingTools
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+@testset "get_design_matrix" begin
+    x = [1,2,1,1,2]
+    y = [0.5,0.6,0.7,0.8,0.9]
+    # class variable
+    X = get_design_matrix(x)
+    @test X == [1.0 0.0; 0.0 1.0; 1.0 0.0; 1.0 0.0; 0.0 1.0]
+    # covariate
+    X = get_design_matrix(x, cov=true)
+    @test X == Matrix(convert.(Float64, x[:,:]))
+    # nested covariate
+    X = get_design_matrix(y, x, cov=true)
+    @test X == [0.5 0.0; 0.0 0.6; 0.7 0.0; 0.8 0.0; 0.0 0.9]
+end
