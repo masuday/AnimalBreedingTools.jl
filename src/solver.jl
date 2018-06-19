0bc9d4e69f0067f07f925641e5c0cb6c63219d89
Yutaka Masuda <yutaka@uga.edu>
2018-06-18 23:50:10 -0400

    minor fix

--- src/solver.jl
+++ src/solver.jl
@@ -1,10 +1,12 @@
 """
-   x = solve_pcg(A,b; maxiter=5000,crit=1e-12)
+   x = solve_pcg(A,b; maxiter=5000,crit=1e-12,verbose=false)
 
 Solve the symmetric positive definite system of equations:
 `A` as sparse left-hand side and `b` as a vector of right-hand side.
 The Jacobi preconditioner (`diag(A)`) is used.
+The maximum number of iterations is specified with `maxiter`.
 Convergence criterion `crit` is `norm(b-A*x)/norm(b)`.
+The details will be shown with `verbose=true`.
 
 
 ```juliadoctest
@@ -12,16 +14,17 @@ julia> x = solve_pcg(A,b)
 ```
 """
 function solve_pcg(A, b::Vector{Tv};
-                   maxiter=5000,crit=1e-15,verbose=false) where {Tv}
+                   maxiter::Int=5000,crit::Float64=1e-15,verbose::Bool=false) where {Tv}
    n = size(A)[1]
-   x = zeros(Tv,n,1)
-   r = zeros(Tv,n,1)
-   p = zeros(Tv,n,1)
-   z = zeros(Tv,n,1)
-   w = zeros(Tv,n,1)
-   Mi = inv(diagm(diag(A)))
+   x = zeros(Tv,n)
+   r = zeros(Tv,n)
+   p = zeros(Tv,n)
+   z = zeros(Tv,n)
+   w = zeros(Tv,n)
+   Mi = Diagonal(1./diag(A))
+   size(A),Tv
    r .= b
-   oldtau = 1
+   oldtau = 1.0
    for i=1:maxiter
       z .= Mi*r
       tau = z'*r
