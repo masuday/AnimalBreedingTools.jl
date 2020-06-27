b3a82923e19cab1e944072e73616a28e05812e70
masuday <masdee@gmail.com>
2020-06-27 10:54:08 -0400

    added built-in data

--- src/AnimalBreedingTools.jl
+++ src/AnimalBreedingTools.jl
@@ -5,9 +5,19 @@ module AnimalBreedingTools
 using Printf
 using LinearAlgebra
 
+struct AnimalBreedingDataSetRaw
+   pedlist::Matrix{Int}
+   genid::Vector{Int}
+   G::Matrix{Float64}
+   y::Vector{Float64}
+   X::Matrix{Float64}
+end
+
 export get_design_matrix, directsum, solve_pcg
+export AnimalBreedingDataSetRaw, load_data_set
 
 include("designmatrix.jl")
 include("solver.jl")
+include("data.jl")
 
 end
