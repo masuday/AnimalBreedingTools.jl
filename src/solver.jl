8e968c767b37a11177014e1c3af44e68e7bbd187
masuday <masdee@gmail.com>
2020-07-07 16:30:05 -0400

    showing precise information

--- src/solver.jl
+++ src/solver.jl
@@ -55,16 +55,23 @@ end
 """
    tr = approximate_trace_of_inverse(A)
 """
-function approximate_trace_of_inverse(A; rounds=1000)
+function approximate_trace_of_inverse(A; rounds=1000, msglev=0)
    if rounds<1
       throw( ArgumentError("negative rounds") )
    end
    tr = Threads.Atomic{Float64}(0.0)
+   done = Threads.Atomic{Int}(0)
    n = size(A,1)
    @threads for iter=1:rounds
+      atomic_add!(done, 1)
       z = [ifelse(rand()>0.5,1.0,-1.0) for i in 1:n]
-      y = solve_pcg(A,z)
       #tr = tr + z'*y
+      if msglev>0
+         @time y = solve_pcg(A,z)
+         println("iter $(done.value) thread $(Threads.threadid())")
+      else
+         y = solve_pcg(A,z)
+      end
       atomic_add!(tr, z'*y)
    end
    return tr.value/rounds
