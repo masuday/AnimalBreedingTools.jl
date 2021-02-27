4704f5f84b763069554ad952b31738d5831fc666
masuday <masdee@gmail.com>
2021-02-27 00:32:01 -0500

    printing vector/matrix with format

--- src/io.jl
+++ src/io.jl
@@ -3,9 +3,12 @@
     disp(x...)
     disp(io::IO,x)
     disp(io::IO,x...)
+    disp(m::Array; format)
+    disp(io::IO, m::Array; format)
 
 Write an object `x` (or objects `x...`) to `io` (or `stdout` if `io` is not given).
 The output is the same as `println` and `Base.print_matrix`, but the element type will not be shown.
+If an array is given, you can specify the C-style format with `format`.
 
 ```juliadoctests
 A = [1 2; 3 4]
@@ -32,11 +35,27 @@ function disp(io::IO,args...)
       disp(io,x)
    end
 end
-function disp(x::Array)
-   Base.print_matrix(stdout,x)
-   print(stdout,"\n")
+function disp(x::Array; format::Union{Nothing,String}=nothing)
+   disp(stdout,x, format=format)
 end
-function disp(io::IO,x::Array)
-   Base.print_matrix(io,x)
-   print(io,"\n")
+function disp(io::IO,x::Array; format::Union{Nothing,String}=nothing)
+   if isnothing(format) || ndims(x)>2
+      Base.print_matrix(io,x)
+      print(io,"\n")
+   elseif ndims(x)==1
+      for i=1:length(x)
+         print_formatted(io,format,x[i])
+         print("\n")
+      end
+   elseif ndims(x)==2
+      for i=1:size(x,1)
+         for j=1:size(x,2)
+            print_formatted(io,format,x[i,j])
+         end
+         print("\n")
+      end
+   end
 end
+
+# see https://discourse.julialang.org/t/printf-with-variable-format-string/3805/3
+print_formatted(io, fmt, args...) = @eval @printf($io, $fmt, $(args...))
