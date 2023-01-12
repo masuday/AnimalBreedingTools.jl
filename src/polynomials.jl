6cee6d4f219b48efd7fb0653172c9b08b731f494
masuday <masdee@gmail.com>
2023-01-12 15:25:12 +0900

    support for arbitrary earliest and latest time points in normalized_legendre_matrix

--- src/polynomials.jl
+++ src/polynomials.jl
@@ -10,9 +10,11 @@ function normalized_legendre(x,n)
 end
 
 """
-    M = normalized_legendre_matrix(t,n)
+    M = normalized_legendre_matrix(t,n; tmin=minimum(t),tmax=maximum(t))
 
 Calculates a matrix with the `n`-th order Legendre coefficients on time points `t`, a vector or range.
+The earliest and latest (minimum and maximum) time points will be taken from the edges of `t`.
+You can provide these time points, `tmin` and `tmax`, as options.
 
 ```juliadoctests
 # up to 3rd order coefficients for 5 to 12
@@ -20,14 +22,18 @@ julia> M = normalized_legendre_matrix(5:12,3)
 
 # same expression
 julia> M = normalized_legendre_matrix([5,6,7,8,9,10,11,12],3)
+
+# as above with time points ranging 1 and 15
+julia> M = normalized_legendre_matrix(5:12,3, tmin=1, tmax=15)
 ```
 """
-function normalized_legendre_matrix(t::Union{UnitRange,Vector},n)
+function normalized_legendre_matrix(t::Union{UnitRange,StepRange,Vector},n; tmin=minimum(t),tmax=maximum(t))
    if !issorted(t)
       throw(ArgumentError("time points not sorted"))
    end
-   tmin = minimum(t)
-   tmax = maximum(t)
+   if tmin > tmax
+      throw(ArgumentError("tmin greater than tmax"))
+   end
    x = -1 .+ 2*( (t .- tmin)/(tmax-tmin) )
    M = [normalized_legendre(y,k) for y=x,k=0:n]
    return M
